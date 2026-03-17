0e5a20a19da7337ed527475ab0d4e956d14304e1
Patricio Del Boca <patriciodelboca@gmail.com>
2026-03-17 10:36:56 +0100

    Add MCP CKAN entrypoint.

--- src/mcp_ckan_examplepythontools/__init__.py
+++ src/mcp_ckan_examplepythontools/__init__.py
@@ -1,2 +1,8 @@
+def register_tools(mcp):
+    @mcp.tool()
+    def greetings_from_examplepythontools():
+        return "Hello from mcp_ckan_examplepythontools!"
+
+
 def main() -> None:
     print("Hello from mcp-ckan-examplepythontools!")
